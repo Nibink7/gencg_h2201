5d0760495d67b5d042ca17a649ce0183c588e2be
Nibink7 <nabil.bhuiyan@stud.hslu.ch>
2022-09-06 09:40:03 +0200

    added day two

--- content/day01/01/sketch.js
+++ content/day01/01/sketch.js
@@ -1,13 +1,64 @@
-let canvas, video;
+let posX;
+let posY;
+let moveX;
+let moveY;
+let amountOfPoints = 100;
+let multiPosX = [];
+let multiPosY = [];
+let veloCityX = [];
+let veloCityY = [];
 
-// Default P5 setup function
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
-    video = createCapture(VIDEO);
-    video.hide();
+  createCanvas(1000, 700);
+  stroke(0,0,0)
+  strokeWeight(0.1)
+  //noLoop()
+  fillArrayWithCoordinates(amountOfPoints)
+  //console.log(multiPosX)
+  
 }
 
-// Default P5 draw loop function 
 function draw() {
-    image(video, 0, 0, width, height);
+  background(240)
+  makePointsAndConnect(amountOfPoints)
 }
+
+/**
+ * @function fillArrayWithCoordinates assigns the X and Y coordinates in their respective position in their array.
+ * @param {*} number 
+ */
+function fillArrayWithCoordinates(number){
+  for(let i = 0; i < number; i++){
+   multiPosX.push(Math.random() * width);
+   multiPosY.push(Math.random() * height);
+   veloCityX.push(Math.random());
+   veloCityY.push(Math.random());
+  }
+}
+
+/**
+ * @function makePointsAndConnect generates points based on the arrays in the @function fillArrayWithCoordinates.
+ * @param {*} number determines the number of the points displayed on the screen.
+ */
+function makePointsAndConnect(number){
+  for(let i = 0; i < number; i++){
+    posX = moveX
+    posY = moveY
+    point(posX,posY)
+
+
+    for(let j = 0; j< number; j++){
+      line(multiPosX[j], multiPosY[j], multiPosX[i], multiPosY[i])
+    }
+    multiPosX[i] = multiPosX[i] + veloCityX[i];
+    multiPosY[i] = multiPosY[i] + veloCityY[i];
+
+    if(multiPosX[i] >= width || multiPosX[i] <= 0){
+      veloCityX[i] = -veloCityX[i];
+    }
+    if(multiPosY[i] >= height || multiPosY[i] <= 0){
+      veloCityY[i] = -veloCityY[i];
+    }
+  }
+}
+
