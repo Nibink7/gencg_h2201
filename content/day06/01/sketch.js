e885f46f6bfcf4de81f7cfe3e3895fd81ca7a489
Nibink7 <nabil.bhuiyan@stud.hslu.ch>
2022-09-12 23:03:18 +0200

    sketch has a face witth mout and eyes

--- content/day06/01/sketch.js
+++ content/day06/01/sketch.js
@@ -1,34 +1,74 @@
-
+let phase = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  rectMode(CENTER)
-  stroke(0)
+  createCanvas(windowWidth, windowHeight);
+  noFill()
+  stroke(255)
 }
 
 function draw() {
-  background(220);
-  faceGen(250)
+  translate(width / 2, height / 2)
+  background(0);
+  head(300)
+  eye(30, 102, 12)
+  eye(30, -102, 12)
+  mouth(70,30,100)
 }
 
-
-function faceGen(headsize){
-  head(headsize)
-  eyes(400,300);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
 
-function head(diameter){
-  circle(width/2,height/2,diameter)
+function head(diameter) {
+  beginShape();
+  for (let i = 0; i < TWO_PI * 4; i += 0.01) {
+    let offsetX = map(cos(i + phase), -1, 1, 0, 4);
+    let offsetY = map(sin(i - phase), -1, 1, 0, 5);
+    let r = map(noise(offsetX, offsetY), 0, 1, diameter * 0.7, diameter * 1.2);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+  phase += 0.0001
 }
 
-function eyes(leftCorner,rightCorner){
-  bezier(50,leftCorner,rightCorner,20, 12,32,55,11)
+function eye(diameter, xPos, yPos) {
+  beginShape();
+  translate(xPos, yPos)
+  for (let i = 0; i < TWO_PI * 4; i += 0.01) {
+    let offsetX = map(cos(i - phase), -1, 1, 0, 4);
+    let offsetY = map(sin(i + phase), -1, 1, 0, 5);
+    let r = map(noise(offsetX, offsetY), 0, 1, diameter * 0.7, diameter * 1.2);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
+    let r2 = map(noise(offsetX, offsetY), 0, 1, diameter * 0.2, diameter * 0.5);
+    let x2 = r2 * cos(i);
+    let y2 = r2 * sin(i);
+    vertex(x2, y2);
+  }
+  endShape(CLOSE);
+  phase += 0.01
 }
 
-function nose(ridgehight, Holes){
+function nose(ridgehight, Holes) {
   circle(holes)
 }
 
-function mouth(mouthsize){
-
+function mouth(diameter, xPos, yPos) {
+  let orbiterX = xPos*phase*1;//random(xPos, xPos+2);
+  let orbiterY = yPos*phase*1;//random(yPos, yPos+2);
+  translate(orbiterX, orbiterY);
+  beginShape();
+  for (let i = 0; i < TWO_PI * 4; i += 0.1) {
+    let offsetX = map(cos(i + phase*3), -1, 1, -10, 4);
+    let offsetY = map(sin(i - phase*3), -1, 1, 0, 6);
+    let r = map(noise(offsetX, offsetY), 0, 1, diameter * 0.7, diameter * 1.2);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+  phase += 0.0001
 }
---
+++ content/day06/day06.md
@@ -0,0 +1,8 @@
+# **Day 6 - 12.09.2022**
+
+The task of today was to create something with faces. I was inspired hereby from the artwork [weird faces]() and tried to do something similar.
+![weird faces](https://digitalideation.github.io/gencg_h2201/notes/day06/images/weird_faces.jpg)
+
+I tried to go with an approach, in which every aspect of the Face will a result of Perlin noise. I also tried to go with morphing it to different faces.
+
+<iframe></iframe>
